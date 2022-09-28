8d6f729466f2cb5eb7303208edc3cc4970cb0afb
AhmadFauzanShururi <114547661+AhmadFauzanShururi@users.noreply.github.com>
2022-09-28 11:05:48 +0700

    Add files via upload

---
+++ latihan.php
@@ -0,0 +1,55 @@
+<html>
+<body background=”1575887.jpg”>
+<center><h1 ><font style=”background-color:black; color:white”>DATA DIRI</h1></center></font>
+</body>
+<style type="text/css">
+
+    img {
+        float : left;
+        border : 2px black;
+        border-radius: 8px;
+        padding: 4px;
+        width : 110;
+    }
+    table {
+        margin-top:10px;
+        background-color : grey;
+       
+    }
+
+
+</style>
+
+</body>
+<?php
+$nim   = 202520029; //string
+$nama     = "Ahmad Fauzan Shururi"; //integer
+$alamat="Kutoarjo"; //decimal
+$alamatemail="hujankecil09@gmail.com";
+$bidangminat="Multi Media";
+?>
+
+<table>
+    <img src="uzan.JPG" alt="">
+<tr>
+	<td>Nama : <?php echo $nama ?>  </td>		
+</tr>
+<tr>
+	<td>NIM : <?php echo $nim ?>  </td>	
+</tr>
+<tr>
+	<td>Alamat : <?php echo $alamat ?>  </td>	
+</tr>
+<tr>
+	<td>Alamat Email : <?php echo $alamatemail ?>  </td>	
+</tr>
+<tr>
+	<td>Bidang Minat: <?php echo $bidangminat ?>  </td>	
+</tr>
+
+
+
+</td>
+</tr>
+</table>
+</html>
